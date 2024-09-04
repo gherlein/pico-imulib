648d3f61538d4bd6fa747c970447440e9de3a7f3
Greg Herlein <gherlein@herlein.com>
2024-09-04 16:58:06 -0700

    updated

--- build/CMakeFiles/pico-imulib.dir/sh2/sh2_util.c.obj.d
+++
@@ -1,4 +0,0 @@
-CMakeFiles/pico-imulib.dir/sh2/sh2_util.c.obj: \
- /home/gherlein/src/pico-projects/pico-imulib/sh2/sh2_util.c \
- /home/gherlein/src/pico-projects/pico-imulib/sh2/sh2_util.h \
- /usr/lib/gcc/arm-none-eabi/13.2.1/include/stdint.h
